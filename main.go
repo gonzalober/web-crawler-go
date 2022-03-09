ea2614c02ba0bfd29606b063b22bc9e99a892dc0
Gonzalo Berhouet <gonzalo.berhouet@ecs.co.uk>
2022-03-09 17:03:14 +0000

    queue fixed

--- main.go
+++ main.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 	"regexp"
+)
+
+var (
+	//channel and concurrency to fix endless loop
+	queue = make(chan string)
 )
 
 func isInDenyList(extension string) bool {
@@ -18,8 +24,10 @@ func isInDenyList(extension string) bool {
 	return false
 }
 
-func crawl( ) {
-	baseurl := "https://github.com/gonzalober"
+func crawl(href string) {
+	fmt.Printf("=======> %v \n", href)
+	// "https://github.com/gonzalober/about"
+	baseurl := href
 	resp, err := http.Get(baseurl)
 	checkError(err)
 
@@ -33,18 +41,60 @@ func crawl( ) {
 	for _, num := range result {
 		str := num[1]
 
+		fmt.Printf("----->>>> %v \n", str[len(str)-3:])
 		if isInDenyList(str[len(str)-3:]) {
 			continue
 		}
-		fmt.Println(num[1])
+		// fmt.Println("----->>>> %v \n" + num[1])
+
+		properUrl := addHostToPath(num[1], href)
+		go func() { queue <- properUrl }()
+		// crawl(addHostToPath(num[1], href))
 	}
 
 	resp.Body.Close()
 
 }
 
+func addHostToPath(path, baseUrl string) string {
+	uri, err := url.Parse(path)
+	if err != nil {
+		return ""
+	}
+	// fmt.Println("URI HOST", uri)
+
+	base, err := url.Parse(baseUrl)
+	if err != nil {
+		return ""
+	}
+	// fmt.Println("BASE HOST", base)
+
+	//takes the host from base, and path from uri
+	//if it has own host do nothing else base.Host+ uri.Path
+	addHostUri := base.ResolveReference(uri)
+
+	return addHostUri.String()
+}
+
 func main() {
-	crawl()
+	arguments := os.Args[1:]
+
+	if len(arguments) == 0 {
+		fmt.Println("Missing URL input")
+		os.Exit(1)
+	}
+	// fmt.Println(arguments)
+
+	//concurrency asyn to not exhaust all the resources
+	go func() {
+		queue <- arguments[0]
+	}()
+	fmt.Printf("--------%v", queue)
+
+	for href := range queue {
+		crawl(href)
+	}
+
 }
 
 func checkError(err error) {
